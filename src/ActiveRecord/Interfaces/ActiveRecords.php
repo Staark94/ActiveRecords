2a77f7f03c9c1aabdc03d444026dd44dfe7025de
Costin Ionut <51002637+Staark94@users.noreply.github.com>
2021-06-22 14:19:28 -0700

    Create ActiveRecords.php

---
+++ src/ActiveRecord/Interfaces/ActiveRecords.php
@@ -0,0 +1,44 @@
+<?php
+namespace Staark\Dashboard\ActiveRecord\Interfaces;
+
+interface ActiveRecords
+{
+    /**
+     * Set connection for model query
+     * @return mixed
+     */
+    static function setConnection(
+        string $driver = NULL, string $host = NULL, string $dbname = NULL,
+        string $user = NULL, string $pass = NULL, string $port = '3307'
+    );
+
+    /**
+     * User or Model store methods
+     * @return mixed
+     */
+    public function save();
+    public function delete();
+    public function insert();
+    public function update();
+
+    /**
+     * User or Model static query methods
+     * @return mixed
+     */
+    static public function find(int $id);
+    static public function findAll();
+    static public function first();
+    static public function findById(int $id);
+    static public function findByEmail(string $email);
+
+    /**
+     * Database query from functions
+     * @param string $sqlQuery
+     * @return mixed
+     */
+    public function select(string $sqlQuery = "");
+    public function from(string $sqlQuery = "");
+    public function where(array $sqlQuery = []);
+    public function limit(int $limit = -1, int $offset = -1);
+    public function get();
+}
